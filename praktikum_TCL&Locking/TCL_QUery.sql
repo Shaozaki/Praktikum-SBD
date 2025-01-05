45ddb65685ddd40734e1e68cf42d17983e030550
Shaozaki <fatihdaffauldzaki@gmail.com>
2025-01-05 17:24:17 +0700

    TCL LOCKING

---
+++ praktikum_TCL&Locking/TCL_QUery.sql
@@ -0,0 +1,29 @@
+-- Active: 1734950767189@@127.0.0.1@3306@rumah_sakit
+USE rumah_sakit;
+--Fatih Daffaul Dzaki
+--24SA11A062
+ SET autocommit = 0;
+ START TRANSACTION;
+ INSERT INTO tbpasien VALUES ('A2348','Rahamat','2003-11-09','Jepara','823999');
+ ROLLBACK;
+  
+  SELECT * FROM tbpasien;
+
+ CREATE USER 'admin1'@'localhost' IDENTIFIED BY 'admin123';
+ GRANT ALL PRIVILEGES ON rumah_sakit.* to 'admin1'@'localhost';
+ REVOKE ALL PRIVILEGES ON rumah_sakit.tbdetailbayar from 'admin1'@'localhost';
+
+--Fatih Daffaul Dzaki
+--24SA11A062
+SET autocommit = 0;
+START TRANSACTION;
+UPDATE tbpasien SET `namaPasien` = 'Rahmat'
+WHERE idpasien = 'A2348';
+SAVEPOINT a;
+INSERT INTO tbpasien VALUES ('A2349','Raya','2009-09-09','Karangraja',084467658);
+SAVEPOINT b;
+INSERT INTO tbpasien VALUES ('A2350','Dainah','2009-07-11','Karangraja',084498643);
+ROLLBACK TO b;
+ROLLBACK TO a;
+
+SELECT * FROM tbpasien;
